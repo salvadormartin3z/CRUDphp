6c0ae727411e7f7e1d5c13bf22c5bdcce4e3944d
Salvador Martinez <67123399+salvadormartin3z@users.noreply.github.com>
2021-09-19 04:43:10 +0200

    Add exercise

---
+++ Ejercicio1.php
@@ -0,0 +1,40 @@
+<?php
+$libros = array(
+    [0] => array(
+        "name" => "La caída de los gigantes",
+        'Cantidad' => 2
+    ),
+    [1] => array(
+        'Nombre' => "apostador",
+        'Cantidad' => 2
+    ),
+    [2] => array(
+        'Nombre' => "El psico analista",
+        'Cantidad' => 2
+    ),
+    [3] => array(
+        'Nombre' => "Viaje al fin de la noche",
+        'Cantidad' => 2
+    ),
+    [4] => array(
+        'Nombre' => "Puente a la realidad",
+        'Cantidad' => 2
+    )
+);
+
+/*
+$Libros = [
+    'La caída de los gigantes' => 2,
+    'apostador' => 2,
+    'El psico analista' => 2,
+    'Viaje al fin de la noche' => 2,
+    'Puente a la realidad' => 2,
+];
+
+function libreria($amount,$name){
+    echo "Nombre: $name,<br> Cantidad: $amount <br><br>"; //Traer tambien el key
+}
+
+//array_walk     iterar con topdo el array
+array_walk($Libros, 'libreria');
+*/
